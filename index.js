b207351604ef4160a6a27db022995b6d09cb428f
Prayag-09 <91512981+Prayag-09@users.noreply.github.com>
2023-11-06 13:58:16 +0530

    Added APIs

---
+++ index.js
@@ -0,0 +1,24 @@
+import Discord from 'discord.js';
+import dotenv from 'dotenv';
+import fs from ('fs');
+import Player from 'discord-player';
+import Rest from '@discordjs/rest';
+import Routes from '@discord-api-types/v9';
+
+dotenv.config()
+const token = process.env.TOKEN;
+
+const loadSlash = process.argv[2] = "load"
+
+const Client_ID = "1170991568740687922"
+const GUILD_ID = "904776353717583942"
+
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_VOICE_STATE"
+    ]
+});
+
+client.loadSlash
